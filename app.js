6efd5f2827477b6b58848637b1d7950b4689013e
soola-sys <agdarbekovsultan@gmail.com>
2023-04-21 17:28:08 +0600

    refactor(extended-2.1): change completed block according to BEM

--- app.js
+++ app.js
@@ -10,9 +10,8 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.querySelector(".section-todo__incomplete-tasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
-
+var incompleteTaskHolder=document.querySelector(".incomplete-tasks");//ul of #incompleteTasks
+var completedTasksHolder=document.querySelector(".completed-tasks");//completed-tasks
 
 //New task list item
 var createNewTaskElement=function(taskString){
